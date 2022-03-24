cedc0b1459dc89e5bdd390e7bfb65c4dd2baffc1
Patrick Selamy <pselamy@gmail.com>
2022-03-24 18:54:10 -0500

    Import BaseEncoding.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 import com.github.pselamy.http.HttpRequest;
 import com.github.pselamy.http.HttpResponse;
+import com.google.common.io.BaseEncoding;
 
 class HelloWorld {
     public static void main(String[] args) {}
