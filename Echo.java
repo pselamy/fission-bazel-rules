e03f93544a88b2a8164bd1f6a3791e58a709acbe
Patrick Selamy <pselamy@gmail.com>
2022-03-24 19:57:08 -0500

    Update and rename HelloWorld.java to Echo.java

--- Echo.java
+++ Echo.java
@@ -4,7 +4,7 @@ import com.github.pselamy.http.HttpRequest;
 import com.github.pselamy.http.HttpResponse;
 import com.google.common.io.BaseEncoding;
 
-class HelloWorld {
+class Echo {
     public static void main(String[] args) {
         String requestStr = args[0];
         HttpRequest request = HttpRequest.parseFrom(base64().decode(requestStr));
