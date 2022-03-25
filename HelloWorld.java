f20b7a8a35d452ff5af08ff795c9e6379e6700b9
Patrick Selamy <pselamy@gmail.com>
2022-03-24 19:38:56 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,14 @@
+import static com.google.common.io.BaseEncoding.base64;
+
 import com.github.pselamy.http.HttpRequest;
 import com.github.pselamy.http.HttpResponse;
 import com.google.common.io.BaseEncoding;
 
 class HelloWorld {
     public static void main(String[] args) {
-        return HttpResponse.getDefaultInstance();
+        String requestStr = args[0];
+        HttpRequest request = HttpRequest.parseFrom(base64().decode(requestStr));
+        HttpResponse response = HttpResponse.newBuilder().setBody(request.body()).build();
+        System.out.writeln(base64().encode(response.toByteArray()));
     }
 }
