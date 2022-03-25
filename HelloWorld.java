e58139f39a4ce1bb48edb85982359d40d9d29e86
Patrick Selamy <pselamy@gmail.com>
2022-03-24 19:53:06 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -8,7 +8,7 @@ class HelloWorld {
     public static void main(String[] args) {
         String requestStr = args[0];
         HttpRequest request = HttpRequest.parseFrom(base64().decode(requestStr));
-        HttpResponse response = HttpResponse.newBuilder().setBody(request.body()).build();
+        HttpResponse response = HttpResponse.newBuilder().setBody(request.getBody()).build();
         System.out.writeln(base64().encode(response.toByteArray()));
     }
 }
