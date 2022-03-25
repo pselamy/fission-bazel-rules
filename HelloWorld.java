266db3024ec46c9232aff700c2e18695b89b8db3
Patrick Selamy <pselamy@gmail.com>
2022-03-24 19:25:45 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,7 @@ import com.github.pselamy.http.HttpResponse;
 import com.google.common.io.BaseEncoding;
 
 class HelloWorld {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        return HttpResponse.getDefaultInstance();
+    }
 }
