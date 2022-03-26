5e5759fb6920ddbe9c96d350f41e1fc4aabea7b9
Patrick Selamy <pselamy@gmail.com>
2022-03-25 20:42:47 -0500

    Delete install.js

--- install.js
+++
@@ -1,9 +0,0 @@
-const njb = require("node-java-connector");
-const jre_version = process.env.JRE_VERSION || 8;
-
-njb
- .install(jre_version, { type: "jre" })
- .then(dir => {})
- .catch(err => {
-  console.log(err);
- });
